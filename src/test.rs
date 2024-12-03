ee1d914e72ab00d4d0a74d00d802c31ecbc96f40
jeancharlesgouleau <jc.gouleau@gmail.com>
2024-12-03 07:23:33 +0100

    feat(src): add src/main.rs, src/plateau.rs, src/test.rs

--- src/test.rs
+++ src/test.rs
@@ -1,106 +1,149 @@
-#[cfg(test)]
-mod tests {
-    use rand::Rng;
+use rand::Rng;
+use crate::plateau::Plateau;
 
-    #[derive(Debug, Clone,PartialEq)]
-    struct Plateau{
-        tiles: Vec<Tile>,
-    }
-    #[derive(Debug, Clone,PartialEq)]
-    struct Tile(i32, i32, i32);
+pub fn create_shuffle_deck() -> Deck {
+    let tiles = vec![
+        new_tiles(1, 2, 3),
+        new_tiles(1, 6, 8),
+        new_tiles(1, 7, 3),
+        new_tiles(1, 6, 3),
+        new_tiles(1, 2, 8),
+        new_tiles(1, 2, 4),
+        new_tiles(1, 7, 4),
+        new_tiles(1, 6, 4),
+        new_tiles(1, 7, 8),
+        new_tiles(5, 2, 3),
+        new_tiles(5, 6, 8),
+        new_tiles(5, 7, 3),
+        new_tiles(5, 6, 3),
+        new_tiles(5, 2, 8),
+        new_tiles(5, 2, 4),
+        new_tiles(5, 7, 4),
+        new_tiles(5, 6, 4),
+        new_tiles(5, 7, 8),
+        new_tiles(9, 2, 3),
+        new_tiles(9, 6, 8),
+        new_tiles(9, 7, 3),
+        new_tiles(9, 6, 3),
+        new_tiles(9, 2, 8),
+        new_tiles(9, 2, 4),
+        new_tiles(9, 7, 4),
+        new_tiles(9, 6, 4),
+        new_tiles(9, 7, 8),
+    ];
 
-    
-    fn new_tiles(x: i32, y: i32, z: i32) -> Tile {
-        Tile(x, y, z)
-    }
-
-    struct Deck{
-    tiles: Vec<Tile>,
+   Deck { tiles}
 }
-    fn create_shuffle_deck() -> Deck {
-        let tiles = vec![
-            new_tiles(1, 2, 3),
-            new_tiles(1, 6, 8),
-            new_tiles(1, 7, 3),
-            new_tiles(1, 6, 3),
-            new_tiles(1, 2, 8),
-            new_tiles(1, 2, 4),
-            new_tiles(1, 7, 4),
-            new_tiles(1, 6, 4),
-            new_tiles(1, 7, 8),
-            new_tiles(5, 2, 3),
-            new_tiles(5, 6, 8),
-            new_tiles(5, 7, 3),
-            new_tiles(5, 6, 3),
-            new_tiles(5, 2, 8),
-            new_tiles(5, 2, 4),
-            new_tiles(5, 7, 4),
-            new_tiles(5, 6, 4),
-            new_tiles(5, 7, 8),
-            new_tiles(9, 2, 3),
-            new_tiles(9, 6, 8),
-            new_tiles(9, 7, 3),
-            new_tiles(9, 6, 3),
-            new_tiles(9, 2, 8),
-            new_tiles(9, 2, 4),
-            new_tiles(9, 7, 4),
-            new_tiles(9, 6, 4),
-            new_tiles(9, 7, 8),
-        ];
 
-        Deck { tiles}
+
+#[derive(Debug, Clone, PartialEq, Copy)]
+pub(crate) struct Tile(pub i32, pub i32, pub i32);
+
+
+fn new_tiles(x: i32, y: i32, z: i32) -> Tile {
+    Tile(x, y, z)
+}
+#[derive(Debug, Clone, PartialEq)]
+pub struct Deck{
+    pub(crate) tiles: Vec<Tile>,
+}
+
+
+pub(crate) fn create_plateau_empty() -> Plateau {
+    Plateau {
+        tiles: vec![Tile(0, 0, 0); 19],
     }
+}
+pub(crate) fn remove_tile_from_deck(deck: &Deck, tile_to_remove: &Tile) -> Deck {
+    // Filtre toutes les tuiles sauf celle à retirer
+    let new_tiles: Vec<Tile> = deck
+        .tiles
+        .iter()
+        .filter(|&tile| tile != tile_to_remove) // Conserve uniquement les tuiles différentes
+        .cloned() // Copie chaque tuile dans le nouveau vecteur
+        .collect();
 
-    fn create_plateau_empty() -> Plateau {
-        Plateau {
-            tiles: vec![Tile(0, 0, 0); 19],
+    Deck { tiles: new_tiles } // Crée un nouveau deck
+}
+pub fn choisir_et_placer(deck: &mut Deck, plateau: &mut Plateau) {
+    let mut rng = rand::thread_rng();
+
+    // Répéter jusqu'à ce que le plateau soit plein
+    while plateau.tiles.contains(&Tile(0, 0, 0)) {
+        // Choisir une tuile aléatoirement dans le deck
+        let deck_len = deck.tiles.len();
+        if deck_len == 0 {
+            break; // Plus de tuiles disponibles
         }
-    }
-    fn remove_tile_from_deck(deck: &Deck, tile_to_remove: &Tile) -> Deck {
-        // Filtre toutes les tuiles sauf celle à retirer
-        let new_tiles: Vec<Tile> = deck
-            .tiles
-            .iter()
-            .filter(|&tile| tile != tile_to_remove) // Conserve uniquement les tuiles différentes
-            .cloned() // Copie chaque tuile dans le nouveau vecteur
-            .collect();
+        let tile_index = rng.gen_range(0..deck_len);
+        let tuile = deck.tiles.remove(tile_index); // Retirer la tuile du deck
 
-        Deck { tiles: new_tiles } // Crée un nouveau deck
-    }
-
-    fn choisir_et_placer(deck: &mut Deck, plateau: &mut Plateau) {
-        let mut rng = rand::thread_rng();
-
-        // Répéter jusqu'à ce que le plateau soit plein
-        while plateau.tiles.contains(&Tile(0, 0, 0)) {
-            // Choisir une tuile aléatoirement dans le deck
-            let deck_len = deck.tiles.len();
-            if deck_len == 0 {
-                break; // Plus de tuiles disponibles
+        // Choisir une position aléatoire dans le plateau
+        let mut position;
+        loop {
+            position = rng.gen_range(0..plateau.tiles.len());
+            if plateau.tiles[position] == Tile(0, 0, 0) {
+                break; // Trouver une case vide
             }
-            let tile_index = rng.gen_range(0..deck_len);
-            let tuile = deck.tiles.remove(tile_index); // Retirer la tuile du deck
-
-            // Choisir une position aléatoire dans le plateau
-            let mut position;
-            loop {
-                position = rng.gen_range(0..plateau.tiles.len());
-                if plateau.tiles[position] == Tile(0, 0, 0) {
-                    break; // Trouver une case vide
-                }
-            }
-
-            // Placer la tuile dans le plateau
-            plateau.tiles[position] = tuile;
         }
-    }
-    fn placer_tile(plateau: &mut Plateau, tuile: Tile, position: usize) -> bool {
-        if plateau.tiles[position] != Tile(0, 0, 0) {
-            return false; // Case déjà occupée
-        }
+
+        // Placer la tuile dans le plateau
         plateau.tiles[position] = tuile;
-        true
     }
+}
+fn placer_tile(plateau: &mut Plateau, tuile: Tile, position: usize) -> bool {
+    if plateau.tiles[position] != Tile(0, 0, 0) {
+        return false; // Case déjà occupée
+    }
+    plateau.tiles[position] = tuile;
+    true
+}
+pub fn result(plateau: &Plateau) -> i32 {
+    let line_indices = vec![
+        (vec![0, 1, 2], 3),
+        (vec![3, 4, 5, 6], 4),
+        (vec![7, 8, 9, 10, 11], 5),
+        (vec![12, 13, 14, 15], 4),
+        (vec![16, 17, 18], 3),
+        (vec![0,3, 7], 3),
+        (vec![1,4, 8,12], 4),
+        (vec![2,5,9,13,16], 5),
+        (vec![6,10, 14,17], 4),
+        (vec![11,15, 18], 3),
+        (vec![7,12, 16], 3),
+        (vec![3,8, 13,17], 4),
+        (vec![0,4,9,14,18], 5),
+        (vec![1,5, 10,15], 4),
+        (vec![2,6, 11], 3),
+    ];
+
+    let mut result = 0;
+
+    for (index_group, multiplier) in line_indices.iter() {
+        if is_equal_group(&plateau.tiles, index_group) {
+            result += plateau.tiles[index_group[0]].0 * *multiplier;
+        }
+    }
+
+    result
+}
+
+/// Helper function to check if all tiles in the group are equal.
+fn is_equal_group(tiles: &[Tile], indices: &[usize]) -> bool {
+    if let Some(first_tile) = tiles.get(indices[0]) {
+        indices.iter().all(|&i| tiles[i].0 == first_tile.0)
+    } else {
+        false
+    }
+}
+#[cfg(test)]
+pub(crate) mod tests {
+    use rand::Rng;
+    use crate::plateau::Plateau;
+    use crate::test::{choisir_et_placer, create_plateau_empty, create_shuffle_deck, Deck, placer_tile, remove_tile_from_deck, result, Tile};
+
+
+
     #[test]
     fn test_placement_tuile_valide_take_it_easy() {
         let mut plateau:Plateau=create_plateau_empty();
@@ -253,33 +296,134 @@ mod tests {
         let point=result(&plateau);
         assert_eq!(point,3);
     }
-    fn result(plateau: &Plateau) -> i32 {
-        let mut result =0;
-        if (plateau.tiles[0].0 == plateau.tiles[1].0) && (plateau.tiles[0].0 == plateau.tiles[2].0) {
-            result=result+ plateau.tiles[0].0*3;
-        }
-        if (plateau.tiles[3].0 == plateau.tiles[4].0)
-            && (plateau.tiles[3].0 == plateau.tiles[5].0)
-            &&(plateau.tiles[3].0 == plateau.tiles[6].0)
-           {
-            result=result+ plateau.tiles[3].0*4;
-        }
-        if (plateau.tiles[7].0 == plateau.tiles[8].0)
-            && (plateau.tiles[7].0 == plateau.tiles[9].0)
-            &&(plateau.tiles[7].0 == plateau.tiles[10].0)
-            &&(plateau.tiles[7].0 == plateau.tiles[11].0)
-        {
-            result=result+ plateau.tiles[7].0*5;
-        }
-        if (plateau.tiles[12].0 == plateau.tiles[13].0)
-            && (plateau.tiles[12].0 == plateau.tiles[14].0)
-            &&(plateau.tiles[12].0 == plateau.tiles[15].0)
-        {
-            result=result+ plateau.tiles[12].0*4;
-        }
-        if (plateau.tiles[16].0 == plateau.tiles[17].0) && (plateau.tiles[16].0 == plateau.tiles[18].0) {
-            result=result+ plateau.tiles[16].0*3;
-        }
-        result
+    #[test]
+    fn test_remplir_plateau_take_it_easy_count_first_diag_3_plateau_3_1() {
+        let mut deck = create_shuffle_deck();
+        let mut plateau = create_plateau_empty();
+        placer_tile(&mut plateau, deck.tiles[0].clone(), 0);
+        placer_tile(&mut plateau, deck.tiles[1].clone(),3);
+        placer_tile(&mut plateau, deck.tiles[2].clone(), 7);
+        let point=result(&plateau);
+        assert_eq!(point,3);
     }
+    #[test]
+    fn test_remplir_plateau_take_it_easy_count_0_3_7() {
+        let mut deck = create_shuffle_deck();
+        let mut plateau = create_plateau_empty();
+        placer_tile(&mut plateau, deck.tiles[0].clone(), 0);
+        placer_tile(&mut plateau, deck.tiles[3].clone(), 3);
+        placer_tile(&mut plateau, deck.tiles[7].clone(), 7);
+        let point = result(&plateau);
+        assert_eq!(point, 3);
+    }
+
+    #[test]
+    fn test_remplir_plateau_take_it_easy_count_1_4_8_12() {
+        let mut deck = create_shuffle_deck();
+        let mut plateau = create_plateau_empty();
+        placer_tile(&mut plateau, deck.tiles[0].clone(), 1);
+        placer_tile(&mut plateau, deck.tiles[1].clone(), 4);
+        placer_tile(&mut plateau, deck.tiles[2].clone(), 8);
+        placer_tile(&mut plateau, deck.tiles[3].clone(), 12);
+        let point = result(&plateau);
+        assert_eq!(point, 4);
+    }
+
+    #[test]
+    fn test_remplir_plateau_take_it_easy_count_2_5_9_13_16() {
+        let mut deck = create_shuffle_deck();
+        let mut plateau = create_plateau_empty();
+        placer_tile(&mut plateau, deck.tiles[0].clone(), 2);
+        placer_tile(&mut plateau, deck.tiles[1].clone(), 5);
+        placer_tile(&mut plateau, deck.tiles[2].clone(), 9);
+        placer_tile(&mut plateau, deck.tiles[3].clone(), 13);
+        placer_tile(&mut plateau, deck.tiles[4].clone(), 16);
+        let point = result(&plateau);
+        assert_eq!(point, 5);
+    }
+
+    #[test]
+    fn test_remplir_plateau_take_it_easy_count_6_10_14_17() {
+        let mut deck = create_shuffle_deck();
+        let mut plateau = create_plateau_empty();
+        placer_tile(&mut plateau, deck.tiles[0].clone(), 6);
+        placer_tile(&mut plateau, deck.tiles[1].clone(), 10);
+        placer_tile(&mut plateau, deck.tiles[2].clone(), 14);
+        placer_tile(&mut plateau, deck.tiles[3].clone(), 17);
+        let point = result(&plateau);
+        assert_eq!(point, 4);
+    }
+
+    #[test]
+    fn test_remplir_plateau_take_it_easy_count_11_15_18() {
+        let mut deck = create_shuffle_deck();
+        let mut plateau = create_plateau_empty();
+        placer_tile(&mut plateau, deck.tiles[0].clone(), 11);
+        placer_tile(&mut plateau, deck.tiles[1].clone(), 15);
+        placer_tile(&mut plateau, deck.tiles[2].clone(), 18);
+        let point = result(&plateau);
+        assert_eq!(point, 3);
+    }
+
+    // Add other tests for all the other specified groups...
+
+    #[test]
+    fn test_remplir_plateau_take_it_easy_count_7_12_16() {
+        let mut deck = create_shuffle_deck();
+        let mut plateau = create_plateau_empty();
+        placer_tile(&mut plateau, deck.tiles[0].clone(), 7);
+        placer_tile(&mut plateau, deck.tiles[1].clone(), 12);
+        placer_tile(&mut plateau, deck.tiles[2].clone(), 16);
+        let point = result(&plateau);
+        assert_eq!(point, 3);
+    }
+
+    #[test]
+    fn test_remplir_plateau_take_it_easy_count_3_8_13_17() {
+        let mut deck = create_shuffle_deck();
+        let mut plateau = create_plateau_empty();
+        placer_tile(&mut plateau, deck.tiles[0].clone(), 3);
+        placer_tile(&mut plateau, deck.tiles[1].clone(), 8);
+        placer_tile(&mut plateau, deck.tiles[2].clone(), 13);
+        placer_tile(&mut plateau, deck.tiles[3].clone(), 17);
+        let point = result(&plateau);
+        assert_eq!(point, 4);
+    }
+
+    #[test]
+    fn test_remplir_plateau_take_it_easy_count_0_4_9_14_18() {
+        let mut deck = create_shuffle_deck();
+        let mut plateau = create_plateau_empty();
+        placer_tile(&mut plateau, deck.tiles[0].clone(), 0);
+        placer_tile(&mut plateau, deck.tiles[1].clone(), 4);
+        placer_tile(&mut plateau, deck.tiles[2].clone(), 9);
+        placer_tile(&mut plateau, deck.tiles[3].clone(), 14);
+        placer_tile(&mut plateau, deck.tiles[4].clone(), 18);
+        let point = result(&plateau);
+        assert_eq!(point, 5);
+    }
+
+    #[test]
+    fn test_remplir_plateau_take_it_easy_count_1_5_10_15() {
+        let mut deck = create_shuffle_deck();
+        let mut plateau = create_plateau_empty();
+        placer_tile(&mut plateau, deck.tiles[0].clone(), 1);
+        placer_tile(&mut plateau, deck.tiles[1].clone(), 5);
+        placer_tile(&mut plateau, deck.tiles[2].clone(), 10);
+        placer_tile(&mut plateau, deck.tiles[3].clone(), 15);
+        let point = result(&plateau);
+        assert_eq!(point, 4);
+    }
+
+    #[test]
+    fn test_remplir_plateau_take_it_easy_count_2_6_11() {
+        let mut deck = create_shuffle_deck();
+        let mut plateau = create_plateau_empty();
+        placer_tile(&mut plateau, deck.tiles[0].clone(), 2);
+        placer_tile(&mut plateau, deck.tiles[1].clone(), 6);
+        placer_tile(&mut plateau, deck.tiles[2].clone(), 11);
+        let point = result(&plateau);
+        assert_eq!(point, 3);
+    }
+
 }
