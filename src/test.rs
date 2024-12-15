9e38b98feadcc399ec03706c4929234fdec041e7
jeancharlesgouleau <jc.gouleau@gmail.com>
2024-12-15 22:00:40 +0100

    feat(src): add src/main.rs, src/result.rs, src/test.rs

--- src/test.rs
+++ src/test.rs
@@ -33,9 +33,6 @@ pub fn create_shuffle_deck() -> Deck {
 
    Deck { tiles}
 }
-
-
-#[cfg(test)]
 #[derive(Debug, Clone, PartialEq)]
 pub struct MCTSNode {
     pub state: GameState,             // Current game state
@@ -54,8 +51,7 @@ pub fn create_mcts_node(state: GameState, parent: Option<*mut MCTSNode>) -> MCTS
     }
 }
 
-
-#[derive(Debug, Clone, PartialEq, Copy)]
+#[derive(Debug, Clone, PartialEq, Copy,Hash,Eq)]
 pub(crate) struct Tile(pub i32, pub i32, pub i32);
 #[derive(Debug, Clone, PartialEq)]
 pub(crate) struct Plateau{
@@ -67,11 +63,15 @@ pub struct GameState {
     pub plateau: Plateau,
     pub deck: Deck,
 }
-
-
 fn new_tiles(x: i32, y: i32, z: i32) -> Tile {
     Tile(x, y, z)
 }
+
+
+
+
+
+
 #[derive(Debug, Clone, PartialEq)]
 pub struct Deck{
     pub(crate) tiles: Vec<Tile>,
@@ -125,51 +125,15 @@ fn placer_tile(plateau: &mut Plateau, tuile: Tile, position: usize) -> bool {
     plateau.tiles[position] = tuile;
     true
 }
-pub fn result(plateau: &Plateau) -> i32 {
-    let mut result = 0;
 
-    // Helper function to calculate the score for a matching set of indices
-    fn calculate_score(indices: &[usize], multiplier: i32, tiles: &[Tile], selector: fn(&Tile) -> i32) -> i32 {
-        let first_value = selector(&tiles[indices[0]]);
-        if indices.iter().all(|&i| selector(&tiles[i]) == first_value) {
-            return first_value * multiplier;
-        }
-        0
-    }
-
-    // Define the patterns for scoring
-    let patterns: Vec<(&[usize], i32, fn(&Tile) -> i32)> = vec![
-        (&[0_usize, 1, 2], 3, |tile: &Tile| tile.0), // Horizontal row 1 based on Tile.0
-        (&[3, 4, 5, 6], 4, |tile: &Tile| tile.0),   // Horizontal row 2 based on Tile.0
-        (&[7, 8, 9, 10, 11], 5, |tile: &Tile| tile.0), // Horizontal row 3 based on Tile.0
-        (&[12, 13, 14, 15], 4, |tile: &Tile| tile.0),  // Horizontal row 4 based on Tile.0
-        (&[16, 17, 18], 3, |tile: &Tile| tile.0),  // Horizontal row 5 based on Tile.0
-        (&[0, 3, 7], 3, |tile: &Tile| tile.1),     // Vertical column 1 based on Tile.1
-        (&[1, 4, 8, 12], 4, |tile: &Tile| tile.1), // Vertical column 2 based on Tile.1
-        (&[2, 5, 9, 13, 16], 5, |tile: &Tile| tile.1), // Vertical column 3 based on Tile.1
-        (&[6, 10, 14, 17], 4, |tile: &Tile| tile.1), // Vertical column 4 based on Tile.1
-        (&[11, 15, 18], 3, |tile: &Tile| tile.1),  // Vertical column 5 based on Tile.1
-        (&[7, 12, 16], 3, |tile: &Tile| tile.2),   // Diagonal from 7 to 16 based on Tile.2
-        (&[3, 8, 13, 17], 4, |tile: &Tile| tile.2), // Diagonal from 3 to 17 based on Tile.2
-        (&[0, 4, 9, 14, 18], 5, |tile: &Tile| tile.2), // Diagonal from 0 to 18 based on Tile.2
-        (&[1, 5, 10, 15], 4, |tile: &Tile| tile.2), // Diagonal from 1 to 15 based on Tile.2
-        (&[2, 6, 11], 3, |tile: &Tile| tile.2),    // Diagonal from 2 to 11 based on Tile.2
-    ];
-
-    // Iterate through the patterns and calculate the score
-    for (indices, multiplier, selector) in patterns {
-        result += calculate_score(indices, multiplier, &plateau.tiles, selector);
-    }
-
-    result
-}
-
+#[cfg(test)]
 pub(crate) mod tests {
 
 
 
     use rand::Rng;
-    use crate::test::{choisir_et_placer, create_mcts_node, create_plateau_empty, create_shuffle_deck, Deck, GameState, MCTSNode, placer_tile, Plateau, remove_tile_from_deck, result, Tile};
+    use crate::result::result;
+    use crate::test::{choisir_et_placer, create_mcts_node, create_plateau_empty, create_shuffle_deck, Deck, GameState, MCTSNode, placer_tile, Plateau, remove_tile_from_deck, Tile};
     #[test]
     fn test_placement_tuile_valide_take_it_easy() {
         let mut plateau:Plateau=create_plateau_empty();
@@ -510,8 +474,9 @@ pub(crate) mod tests {
                 deck: new_deck,
             })
 
-            }
+        }
     }
+
 
     #[test]
     fn test_simulate() {
@@ -824,9 +789,18 @@ pub(crate) mod tests {
                 let position = legal_moves[rng.gen_range(0..legal_moves.len())];
                 let tile_index = rng.gen_range(0..selected_node.state.deck.tiles.len());
                 let chosen_tile = selected_node.state.deck.tiles[tile_index];
+                let deck_size_before = selected_node.state.deck.tiles.len();
+                println!("Deck size before expansion: {}", deck_size_before);
+
                 if let Some(new_state) = apply_move(selected_node.state.clone(), chosen_tile, position) {
-                    expand(selected_node.clone(), new_state);
+                    expand(selected_node.clone(), new_state.clone());
+
+                    let deck_size_after = new_state.deck.tiles.len();
+                    println!("Deck size after expansion: {}", deck_size_after);
+
+                    assert_eq!(deck_size_after, deck_size_before - 1, "Deck size did not decrement correctly!");
                 }
+
             }
 
             // Simulation
