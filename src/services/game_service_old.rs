b0c9f7497433f9446270a84c8292e9fd9ef14075
jeancharlesgouleau <jc.gouleau@gmail.com>
2025-09-21 21:36:25 +0200

    chore(docs+services): remove 17 files in docs & services

--- src/services/game_service_old.rs
+++
@@ -1,8 +0,0 @@
-// src/services/game_service.rs - Service de jeu refactorisé en modules
-
-// Réexport du module principal
-pub use self::game_service_module::*;
-
-// Le module principal est maintenant dans game_service/
-#[path = "game_service/mod.rs"]
-mod game_service_module;
